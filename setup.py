9d0ea585d8e297c395b720b24de253793f666513
Matthew Harrington <Mat.Harrington.Work@gmail.com>
2017-10-10 14:07:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def package_data(pkg, roots):
 
 setup(
     name='captionedimage',
-    version='1.0',
+    version='1.1',
     description='Captioned Image Xblock',   
     license='AGPL v3',         
     packages=[
